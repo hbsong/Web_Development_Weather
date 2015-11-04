f5c0a3cf74cb01f58c754465b4a67cfe880b212b
YubPan <yb.pan@yahoo.com>
2015-11-04 18:24:05 -0500

    modified php with clear by Bj

--- backSave.php
+++ backSave.php
@@ -2,9 +2,14 @@
 
 $q = $_REQUEST["q"];
 
-$myfile = fopen("backSave.txt", "w") or die("Unable to open file!");
+if (strpos($q,'Beijing') !== false) {
+    $myfile = fopen("backSave.txt", "w") or die("Unable to open file!");;
+} else {
+	$myfile = fopen("backSave.txt", "a") or die("Unable to open file!");
+}
+
 $txt = $_REQUEST["q"];
-fwrite($myfile, $txt);
+fwrite($myfile, $txt . "\n");
 fclose($myfile);
 
 ?>  
